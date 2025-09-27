6b133c3d9053a6bb7d485970865c40e8acf46f0a
italo <italocaua19506@gmail.com>
2025-09-27 20:57:05 -0300

    User signup method implemented

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+import * as argon from 'argon2'
 
 @Injectable()
 export class UserService {
 
+    constructor(private prisma: PrismaService){}
+
     async signup(dto: CreateUserDto){
         
+        const hash = await argon.hash(dto.password);
+
+        try{
+            const user = await this.prisma.user.create({
+                data: {
+                    name: dto.name,
+                    email: dto.email,
+                    password: hash,
+                }
+            });
+        }
+        catch(error){
+            throw error;
+        }
     }
 }
